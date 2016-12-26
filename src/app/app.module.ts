85f840e7da6983ce8496ab1ae16b4a43d7806697
Steve Zheng <steve.zheng@globallogic.com>
2016-12-26 09:14:54 -0500

    update ng2cli and files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { DeviceModule } from './device/device.module';
 import { AppRoutingModule } from './app-routing.module';
-import { CartModule } from './cart/cart.module';
 import { CommentModule } from './comment/comment.module';
-import { ApiService } from './shared/services/api/api.service';
 import { MoneyPipe } from './shared/pipes/money/money.pipe';
 
 @NgModule({
@@ -20,11 +16,9 @@ import { MoneyPipe } from './shared/pipes/money/money.pipe';
     BrowserModule,
     HttpModule,
     AppRoutingModule,
-    DeviceModule,
-    CartModule,
     CommentModule
   ],
-  providers: [ApiService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
